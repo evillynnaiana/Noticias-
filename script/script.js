56ada434a44b754b9803c7c46726f5b627eecbd5
Evillyn Naiana <evillynnaiana@hotmail.com>
2023-01-25 10:55:58 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,18 @@
+const $$ = function (id) {
+    return document.getElementById(id);
+}
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
+
+function next() {
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev() {
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+setInterval(next, 5000);
